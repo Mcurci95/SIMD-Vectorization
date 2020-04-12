5b62ccdecc902c3b0d6453e66f4417e677f3a78d
mcurci95 <mcurci95@gmail.com>
2020-04-12 16:36:28 -0700

    Min function for ints using SIMD

--- min.c
+++ min.c
@@ -5,11 +5,11 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-#define MAX_LEN 10
+#define MAX_LEN 120000
 
 
-float min_naive(size_t n, float *a) {
-    float min = a[0];
+int min_naive(size_t n, int *a) {
+    int min = a[0];
 
     for (int i=0; i < n; i++) {
         if (a[i] < min)
@@ -20,28 +20,52 @@ float min_naive(size_t n, float *a) {
 }
 
 
-// Vectorized example
-float min_v(__m256 a) {
-    __m256 simd_min = a;
+// Using vectorization
+int min_v(size_t SIZE, int  *a) {
+    __m128i simd_min_comparison;
+ 
+    // Start with 4 offset
+    __m128i simd_min = _mm_loadu_si128((__m128i *) &a[0]); 
 
-    simd_min = _mm_min_epi8(simd_min, _mm_extract_epi8(simd_min,simd_min,1));
-    simd_min = _mm_min_epi8(simd_min, _mm_extract_epi8(simd_min,simd_min,2));
-    simd_min = _mm_min_epi8(simd_min, _mm_extract_epi8(simd_min,simd_min,4));
-    simd_min = _mm_min_epi8(simd_min, _mm_extract_epi8(simd_min,simd_min,8));
+    int bitmax[4];
 
-    return simd_min;
+    for (int i = 1; i < SIZE; i += 4) {
+        simd_min_comparison = _mm_loadu_si128((__m128i *) &a[i]);
+
+        __m128i simd_min = _mm_min_epi16(simd_min_comparison, simd_min);
+    }
+    
+    _mm_storeu_si128((__m128i *)bitmax, simd_min);
+    return min_naive(4, bitmax);
 }
 
-int main() {
-    float arr[MAX_LEN];
-    int n = 10;
-    for(int i = 0; i < MAX_LEN; i++)
-        arr[i] = (float) i;
-    float min_value = min_naive(n, arr);
-    printf("%f", min_value);
+int main()
+{
 
-    // SIMD
-    __m256 arr1 = _mm256_set_ps(1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0);
-    __m256 simd_min = min_v(arr1);
-    
+    	int arr[MAX_LEN];
+    	for (int i = 0; i < MAX_LEN; i++)
+        	arr[i] = i;
+
+	double time_spent1 = 0.0;
+	double time_spent2 = 0.0;
+	clock_t begin = clock();
+
+	printf("min = %d\n", min_naive(MAX_LEN, arr));
+
+	clock_t end = clock();
+	time_spent1 += (double)(end - begin) / CLOCKS_PER_SEC;
+
+	printf("Time elpased is %f seconds for naive min.\n", time_spent1);
+
+    	begin = clock();
+
+	printf("min = %d\n", min_v(MAX_LEN, arr));
+
+    	end = clock();
+	time_spent2 += (double)(end - begin) / CLOCKS_PER_SEC;
+
+    	printf("Time elpased is %f seconds for 128bit vectorize min\n", time_spent2);
+
+	printf("%f X time faster.\n", time_spent1/time_spent2);
+	return 0;
 }
