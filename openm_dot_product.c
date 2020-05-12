806dc20c6455f154fdbecc687086051918a4d7be
mcurci95 <mcurci95@gmail.com>
2020-05-11 23:39:11 -0700

    Parallelizing dot_product

--- openm_dot_product.c
+++ openm_dot_product.c
@@ -1,5 +1,7 @@
-#include <stdio.h>
 
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 // #define  120000
 
@@ -11,10 +13,24 @@ int dot_product_naive(size_t N, int *a, int *b) {
     return product;
 }
 
+int dot_product(size_t N, int *a, int *b) {
+    int product;
+
+    #pragma omp parallel for reduction(+:product)
+    for (size_t i = 0; i < N; i++) {
+        product += a[i] * b[i];
+    }
+
+    return product;
+}
+
 int main() {
     int a[] =  {1,2,3,4};
     int b[] = {1,2,3,4};
     size_t n = 4;
     int product = dot_product_naive(n, &a, &b);
-    printf("%d", product);
+    printf("%d\n", product);
+
+    int new_product = dot_product(n, &a, &b);
+    printf("%d\n", new_product);
 }
