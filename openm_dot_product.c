98c1b6159da4896da74ac95dce820b269056fe50
mcurci95 <mcurci95@gmail.com>
2020-05-15 23:10:24 -0700

    Adding ordered parameter for parallel computations

--- openm_dot_product.c
+++ openm_dot_product.c
@@ -18,7 +18,7 @@ int dot_product(size_t N, int *a, int *b) {
     int product = 0;
     
 
-    #pragma omp parallel for reduction(+:product)
+    #pragma omp parallel for ordered reduction(+:product)
     for (size_t i = 0; i < N; i++) {
         product += a[i] * b[i];
     }
@@ -40,8 +40,7 @@ int main() {
         a[i] = 1;
         b[i] = 1;
     }
-    // int a[] =  {1,2,3,4};
-    // int b[] =  {1,2,3,4};
+
 
     clock_t begin = clock();
     int product = dot_product_naive(N, &a, &b);
@@ -58,7 +57,7 @@ int main() {
     end = clock();
 	time_spent2 += (double)(end - begin) / CLOCKS_PER_SEC;
 
-    	printf("Time elapsed is %f seconds for OPENMP parallel dot product\n", time_spent2);
+    printf("Time elapsed is %f seconds for OPENMP parallel dot product\n", time_spent2);
 
 	printf("%f X time faster.\n", time_spent1/time_spent2);
     
